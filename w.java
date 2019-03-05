6acb9e46603cac02f6d8e129519d577cda6e2fbc
kamau0001 <chegejohn159@gmail.com>
2019-03-05 13:16:21 +0300

    java  py

---
+++ w.java
@@ -0,0 +1,17 @@
+
+class f{
+ int s(){
+        int sum=0;
+for(int x=0;x<11;x++){
+        sum=sum+x;
+        
+}
+return sum;
+}
+}
+ public class w{
+public static void main(String[]args){
+               
+        f p=new f();
+System.out.print(p.s());
+      }}  
---
+++ w.py
@@ -0,0 +1,7 @@
+def f():
+     sum=0
+     for x in range (11):
+         sum=sum+x
+
+     return sum
+print(f())       
